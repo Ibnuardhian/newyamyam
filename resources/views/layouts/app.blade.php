7aad9c087a483e88a2da91d40cae1db5407be19b
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-01-20 11:58:35 +0700

    update environment configuration for production database; adjust stylesheet link and add Content Security Policy meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,8 @@
     @include('includes.style')
     @stack('addon-style')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="{{ env('NGROK_DOMAIN') }}/style/main.css">
+    <link rel="stylesheet" href="/style/main.css">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
   </head>
 
